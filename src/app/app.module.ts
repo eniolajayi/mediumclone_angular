d5373b681efa01ab26b4a66909933cd6cc60dcd9
eniolajayi <ajayieniola16@gmail.com>
2023-09-05 05:55:23 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
