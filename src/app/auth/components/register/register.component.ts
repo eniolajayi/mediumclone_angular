7e476abe0c662e9876be14d2582853121b626f98
eniolajayi <ajayieniola16@gmail.com>
2023-09-06 18:01:04 +0100

    add isSubmitting selector

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -3,13 +3,16 @@ import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
 import {Store} from '@ngrx/store'
 import {register} from '../../store/action'
 import {RegisterRequestInterface} from '../../types/registerRequest.interface'
-import { RouterLink } from '@angular/router'
+import {RouterLink} from '@angular/router'
+import {selectIsSubmitting} from '../../store/reducers'
+import {AuthStateInterface} from '../../types/authState.interface'
+import {CommonModule} from '@angular/common'
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
@@ -17,7 +20,13 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   })
-  constructor(private fb: FormBuilder, private store: Store) {}
+
+  isSubmitting$ = this.store.select(selectIsSubmitting)
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<{auth: AuthStateInterface}>
+  ) {}
 
   onSubmit() {
     console.log('form', this.form.getRawValue())
